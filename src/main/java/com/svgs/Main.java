dc4f9f0bf1ca0e5bbcf819d230a1f2b0e6e5f5c9
Noah-DestroyerOfWorlds <noahfosterbrown@gmail.com>
2024-09-24 10:02:38 -0400

    24092024

--- src/main/java/com/svgs/Main.java
+++ src/main/java/com/svgs/Main.java
@@ -1,10 +1,27 @@
 package com.svgs;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
         String url = "jdbc:sqlite:./src/main/resources/chinook.db";
+        try{
+            Connection conn = DriverManager.getConnection(url);
+            Statement state = conn.createStatement();
+            String query = "SELECT * FROM employees ORDER BY FirstName ASC";
+            ResultSet results = state.executeQuery(query);
+            while(results.next()){
+                String first = results.getString("FirstName");
+                System.out.println(first);
+            }
 
-
+            conn.close();
+        }catch(SQLException e){
+            System.out.println(e);
+        }
     }
 }
